0b4bb5306a0dba1d5e62f4a836a96c9cf7c7b2d9
pyama86 <pyama@pepabo.com>
2017-04-06 16:24:55 +0900

    Immediately unpack

--- recipes/naxsi.rb
+++ recipes/naxsi.rb
@@ -5,7 +5,7 @@ cache_path = Chef::Config[:file_cache_path]
 
 remote_file "/usr/local/src/naxsi-#{node['nginxxx']['naxsi']['version']}.tar.gz" do
   source "https://github.com/nbs-system/naxsi/archive/#{node['nginxxx']['naxsi']['version']}.tar.gz"
-  notifies :run, 'execute[unpack_naxsi]'
+  notifies :run, 'execute[unpack_naxsi]', :immediately
 end
 
 execute 'unpack_naxsi' do
