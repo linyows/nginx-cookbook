e8ea629b2aeaca88b90a6dfd59a624c81f01aa3d
linyows <linyows@gmail.com>
2017-05-25 18:36:03 +0900

    bump version to 0.9.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'linyows@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.0'
+version          '0.9.0'
 
 depends 'build-essential'
 depends 'yum'
